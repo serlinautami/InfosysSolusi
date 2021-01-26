070a521070c06d963e251b2d926348df264c7fa2
Serlina Utami <46602473+serlinautami@users.noreply.github.com>
2021-01-26 21:33:33 +0700

    Add files via upload

---
+++ KalkulatorSerli.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulatorserli;
+
+/**
+ *
+ * @author SerlinaUtami
+ */
+public class KalkulatorSerli {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        kalkulator a = new kalkulator();
+        a.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
